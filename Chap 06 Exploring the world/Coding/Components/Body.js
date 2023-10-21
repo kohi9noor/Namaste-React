a0c770163fb4bf262ca8cdd6e9f0e11507f0c115
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2023-10-21 18:02:04 +0200

    add config

---
+++ Chap 06 Exploring the world/Coding/Components/Body.js
@@ -0,0 +1,50 @@
+import { restaurantList } from "../constants";
+import RestauratCard from "../Components/RestaurantCard"
+import { useState } from "react";
+
+function filterData(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) =>{
+        restaurant.data?.name.toLowerCase().includes(searchText.toLowerCase())
+    });
+
+    return filterData;
+}
+
+const Body = () => {
+    // useState: To create a state variable, searchText is local state variable
+    const [searchText, setSearchText] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
+    return (
+      <>
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search a restaurant you want..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          ></input>
+          <button
+            className="search-btn"
+            onClick={() => {
+              // filter the data
+              const data = filterData(searchText, restaurants);
+              // update the state of restaurants list
+              setRestaurants(data);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="restaurant-list">
+          {restaurants.map((restaurant) => {
+            return (
+              <RestauratCard key={restaurant.data.id} {...restaurant.data} />
+            );
+          })}
+        </div>
+      </>
+    );
+  };
+  
+  export default Body;
---
+++ Chap 06 Exploring the world/Coding/Components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div className="footer">
+         Created By
+        <i className="fa-solid fa-heart"></i>
+        <a href="https://t.me/@AdvanceMonk" target="_blank">
+          kohinoor nimes 
+        </a>
+        <i className="fa-solid fa-copyright"></i>2023
+        <strong>
+          Food<span>Fire</span>
+        </strong>
+      </div>
+    );
+  };
+  
+  export default Footer;
